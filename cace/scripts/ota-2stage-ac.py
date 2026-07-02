b612b9bb9b97fd860a9528bf602fd6347ba1436b
Sriram <sriramrepaka29@gmail.com>
2026-07-02 09:32:03 +0200

    "More plots for LPF"

--- cace/scripts/ota-2stage-ac.py
+++ cace/scripts/ota-2stage-ac.py
@@ -22,20 +22,37 @@ def postprocess(results: dict[str, list], conditions: dict[str, Any]) -> dict[st
     gain_mc_arr = [val for val in gain_mc_arr if 0.1 <= val <= 10]
     print(f'gain_mc_arr = {gain_mc_arr}')
     
-    # Iterate over bw MC results
-    bw_mc_arr = []
-    for bw_mc in results['bw_mc']:
-        bw_mc_arr.append(bw_mc)
-    print(f'bw_mc_arr = {bw_mc_arr}')
+    # 2. Process Frequency Metric (Dynamic Check)
+    freq_mc_arr = []
     
-    # Delete statistical outliers in bw_mc_arr
-    bw_mc_arr = [val for val in bw_mc_arr if 10e3 <= val <= 10e9]
-    print(f'bw_mc_arr = {bw_mc_arr}')
-    
-    # Save data as .csv for later use
-    np.savetxt('cace/scripts/ota-2stage-ac.csv', 
-        np.column_stack((np.array(gain_mc_arr), np.array(bw_mc_arr))), comments = "", 
-        header = "gain_mc_arr,bw_mc_arr", delimiter = ",")
+    # Check if this is the Voltage Buffer Testbench (looking for 'bw_mc')
+    if 'bw_mc' in results:
+        for bw_mc in results['bw_mc']:
+            freq_mc_arr.append(bw_mc)
+        # Outlier filter for wideband buffer (10 kHz to 10 GHz)
+        freq_mc_arr = [val for val in freq_mc_arr if 10e3 <= val <= 10e9]
+        header_str = "gain_mc_arr,bw_mc_arr"
+        return_dict = {'gain_mc_arr': gain_mc_arr, 'bw_mc_arr': freq_mc_arr}
+        csv_filename = 'cace/scripts/ota-2stage-ac.csv'
 
-    return {'gain_mc_arr': gain_mc_arr, 'bw_mc_arr': bw_mc_arr}
+    # Otherwise, check if this is the LPF Testbench (looking for 'fc_mc')
+    elif 'fc_mc' in results:
+        for fc_mc in results['fc_mc']:
+            freq_mc_arr.append(fc_mc)
+        # Outlier filter for narrow 10 kHz audio filter band (1 kHz to 100 kHz)
+        freq_mc_arr = [val for val in freq_mc_arr if 1e3 <= val <= 100e3]
+        header_str = "gain_mc_arr,fc_mc_arr"
+        return_dict = {'gain_mc_arr': gain_mc_arr, 'fc_mc_arr': freq_mc_arr}
+        csv_filename = 'cace/scripts/lpf-2stage-ac.csv'
+        
+    else:
+        print("Warning: No matching frequency metric found in results dictionary.")
+        return {'gain_mc_arr': gain_mc_arr}
+
+    # 3. Save the data to the correct CSV file path
+    np.savetxt(csv_filename, 
+        np.column_stack((np.array(gain_mc_arr), np.array(freq_mc_arr))), comments = "", 
+        header = header_str, delimiter = ",")
+
+    return return_dict
 # ============================================
